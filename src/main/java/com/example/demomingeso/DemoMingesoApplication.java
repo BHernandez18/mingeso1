81e9a217ef5873ecc11e77588253cb0a93361646
Benjamín Hernández <benjamin.hernandez@usach.cl>
2017-09-22 22:52:38 -0300

    Se añaden archivos restantes

---
+++ src/main/java/com/example/demomingeso/DemoMingesoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demomingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMingesoApplication.class, args);
+	}
+}
